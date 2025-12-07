87781adf1fed5fb6f04447a873af8b22445f8df8
BALDEV <baldevbaraiya18@gmail.com>
2025-12-07 09:29:59 +0530

    commit

---
+++ assign3.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+
+void main()
+{
+  print("enter number:");
+  var num=int.parse(stdin.readLineSync().toString());
+
+  if(num % 2==0) {
+    print("$num is odd");
+  }
+  else
+  {
+    print("$num is even");
+  }
+}
