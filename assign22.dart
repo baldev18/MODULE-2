719313b4986389246afad703ce32f4c76e82e6e7
BALDEV <baldevbaraiya18@gmail.com>
2025-12-09 06:37:09 +0100

    commit

---
+++ assign22.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main()
+{
+  try
+  {
+    File f=File("b.txt");
+    f.writeAsStringSync("bafbb");
+  }
+  catch(b)
+  {
+    print(b);
+  }
+  finally
+      {
+        print("done");
+      }
+}
